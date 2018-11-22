208c83588f146570b97447594695901b574500f4
Alexey Zaytsev <alex.rv9rh@gmail.com>
2018-11-22 13:18:29 +0500

    add copy 'file-dir', small refactoring in smartphone_backup_test.py

--- smartphone_backup_test.py
+++ smartphone_backup_test.py
@@ -8,30 +8,69 @@ import shutil
 
 tzutc = tzutc()
 
-local_path_1 = r"test_dir_1\myfile.txt"
-local_path_2 = r"test_dir_2\myfile.txt"
-
 def copy_with_replace_by_date(path_from, path_to):
 	if not os.path.exists(path_from):
 		print("Path 'from' not found!")
 		return False
 	
+	# file-file:
 	if os.path.isfile(path_from):
-		path_from_date_mod = int(os.path.getmtime(path_from))
-		path_from_date_mod = datetime.datetime.fromtimestamp(path_from_date_mod)
-		path_from_date_mod = path_from_date_mod.astimezone(tzutc)
+		path_from_mtime = int(os.path.getmtime(path_from))
+		path_from_mtime = datetime.datetime.fromtimestamp(path_from_mtime)
+		path_from_mtime = path_from_mtime.astimezone(tzutc)
 		
 		if os.path.exists(path_to):
-			if os.path.isfile(path_to):		
-				path_to_date_mod = int(os.path.getmtime(path_to))
-				path_to_date_mod = datetime.datetime.fromtimestamp(path_to_date_mod)
-				path_to_date_mod = path_to_date_mod.astimezone(tzutc)
+			if os.path.isfile(path_to):
+				print("file-file")
+				path_to_mtime = int(os.path.getmtime(path_to))
+				path_to_mtime = datetime.datetime.fromtimestamp(path_to_mtime)
+				path_to_mtime = path_to_mtime.astimezone(tzutc)
 				
-				if path_from_date_mod > path_to_date_mod:
-					print("rewrite")
+				if path_from_mtime > path_to_mtime:
+					print("file-file: rewrite")
 					shutil.copy(path_from, path_to)
 			
+			# file-dir:
+			elif os.path.isdir(path_to):
+				print("file-dir")
+				file_name = os.path.basename(path_from)
+				p = os.path.join(path_to, file_name)
+				p = os.path.normpath(p)
+				# print(p)
+				
+				if os.path.exists(p):
+					p_mtime = int(os.path.getmtime(p))
+					p_mtime = datetime.datetime.fromtimestamp(p_mtime)
+					p_mtime = p_mtime.astimezone(tzutc)
+					
+					if path_from_mtime > p_mtime:
+						print("file-dir: rewrite")
+						shutil.copy(path_from, p)
+						
+				else:
+					print("file-dir: write")
+					shutil.copy(path_from, p)
+					
+		else:
+			# !!! если путь_куда - файл, то создаст каталог с именем этого файла (неправильно)
+			# решил сделать другую версию(ветку) с третьим параметром, указывающим тип путей (ff, fd, df, dd)
+			# мне кажется так будет более очевидное поведение программы и явная остановка, при ошибке ввода.
+			print("file-dir: create path and write")
+			os.makedirs(path_to)
+			file_name = os.path.basename(path_from)
+			p = os.path.join(path_to, file_name)
+			p = os.path.normpath(p)
+			# print(p)
+			shutil.copy(path_from, p)
+			
 	return True
-		
-a = copy_with_replace_by_date(local_path_1, local_path_2)	
+
+dir_from_f1 = r"test_dir_from\myfile.txt"
+dir_from_f2 = r"test_dir_from\myfile2.txt"
+dir_to_f1 = r"test_dir_to\myfile.txt"
+dir_from = r"test_dir_from"
+dir_to = r"test_dir_to"
+dir_to_d3 = r"test_dir_to\dir_3"
+
+a = copy_with_replace_by_date(dir_from_f1, dir_to_f1)	
 print(a)
