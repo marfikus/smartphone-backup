643d62fe80a654703f64eff3e89e009b012dc8d0
Alexey Zaytsev <alex.rv9rh@gmail.com>
2018-11-30 17:27:27 +0500

    added copy 'dir-dir' in smartphone_backup_test.py; not tested!

--- smartphone_backup_test.py
+++ smartphone_backup_test.py
@@ -74,25 +74,25 @@ def copy_with_replace_by_date(path_from, path_to, op_type):
 				return {"status": status, "msg": msg}
 			
 			file_name = os.path.basename(path_from)
-			p = os.path.join(path_to, file_name)
-			p = os.path.normpath(p)
-			# print(p)
+			path_to_new = os.path.join(path_to, file_name)
+			path_to_new = os.path.normpath(path_to_new)
+			# print(path_to_new)
 			
-			if os.path.exists(p):
+			if os.path.exists(path_to_new):
 				path_from_mtime = int(os.path.getmtime(path_from))
 				path_from_mtime = datetime.datetime.fromtimestamp(path_from_mtime)
 				path_from_mtime = path_from_mtime.astimezone(tzutc)
 			
-				p_mtime = int(os.path.getmtime(p))
-				p_mtime = datetime.datetime.fromtimestamp(p_mtime)
-				p_mtime = p_mtime.astimezone(tzutc)
+				path_to_new_mtime = int(os.path.getmtime(path_to_new))
+				path_to_new_mtime = datetime.datetime.fromtimestamp(path_to_new_mtime)
+				path_to_new_mtime = path_to_new_mtime.astimezone(tzutc)
 				
-				if path_from_mtime > p_mtime:
+				if path_from_mtime > path_to_new_mtime:
 					print("file-dir: rewrite")
-					shutil.copy(path_from, p)
+					shutil.copy(path_from, path_to_new)
 			else:
 				print("file-dir: write")
-				shutil.copy(path_from, p)
+				shutil.copy(path_from, path_to_new)
 		else:
 			print("'path_to' not exists")
 			print("Create path: ", path_to)
@@ -108,7 +108,33 @@ def copy_with_replace_by_date(path_from, path_to, op_type):
 		
 	elif op_type == "dd":
 		print("dir-dir")
-		pass
+		if not os.path.isdir(path_from):
+			status = "Error!"
+			msg = "Path 'from' is not a directory: '{}'".format(path_from)
+			return {"status": status, "msg": msg}
+
+		list_path_from = os.listdir(path_from)
+		print(list_path_from)
+		print(len(list_path_from))
+		for obj in list_path_from:
+			print(obj)
+			path_from_obj = os.path.join(path_from, obj)
+			path_from_obj = os.path.normpath(path_from_obj)
+			if os.path.isfile(path_from_obj):
+				print("file")
+				res = copy_with_replace_by_date(path_from_obj, path_to, "fd")
+				print(res)
+			elif os.path.isdir(path_from_obj):
+				print("dir")
+				path_to_obj = os.path.join(path_to, obj)
+				path_to_obj = os.path.normpath(path_to_obj)
+				res = copy_with_replace_by_date(path_from_obj, path_to_obj, "dd")
+				print(res)
+			else:
+				print("The object is not supported: ", path_from_obj)
+			print("======================")
+			
+			
 	else:
 		status = "Error!"
 		msg = "Operation type is undefined: '{}'".format(op_type)
@@ -127,7 +153,7 @@ dir_to_d3 = r"test_dir_to\dir_3"
 dir_to_d3_er = "test_dir_to\dir_3\\"
 dir_to_d3_f1 = r"test_dir_to\dir_3\myfile.txt"
 
-a = copy_with_replace_by_date(dir_from_f1, dir_to_f1, "ff")
+a = copy_with_replace_by_date(".", dir_to_f1, "dd")
 print(a)
 
 # add copied files counter
