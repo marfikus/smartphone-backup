c890e0001885172189c092d0f52524839676f28a
Alexey Zaytsev <alex.rv9rh@gmail.com>
2018-12-04 14:32:28 +0500

    added: copied files counter in smartphone_backup_test.py; tested

--- smartphone_backup_test.py
+++ smartphone_backup_test.py
@@ -11,24 +11,25 @@ tzutc = tzutc()
 def copy_with_replace_by_date(path_from, path_to, op_type):
 	status = "Ok"
 	msg = ""
+	copied_files = 0
 	
 	if not os.path.exists(path_from):
 		status = "Error!"
 		msg = "Path 'from' not found: '{}'".format(path_from)
-		return {"status": status, "msg": msg}
+		return {"status": status, "msg": msg, "copied_files": copied_files}
 	
 	if op_type == "ff":
 		print("file-file")
 		if not os.path.isfile(path_from):
 			status = "Error!"
 			msg = "Path 'from' is not a file: '{}'".format(path_from)
-			return {"status": status, "msg": msg}
+			return {"status": status, "msg": msg, "copied_files": copied_files}
 		
 		if os.path.exists(path_to):
 			if not os.path.isfile(path_to):
 				status = "Error!"
 				msg = "Path 'to' is not a file: '{}'".format(path_to)
-				return {"status": status, "msg": msg}
+				return {"status": status, "msg": msg, "copied_files": copied_files}
 			
 			mtime_path_from = int(os.path.getmtime(path_from))
 			mtime_path_from = datetime.datetime.fromtimestamp(mtime_path_from)
@@ -40,38 +41,40 @@ def copy_with_replace_by_date(path_from, path_to, op_type):
 			
 			if mtime_path_from > mtime_path_to:
 				print("file-file: rewrite")
-				shutil.copy(path_from, path_to)	
+				shutil.copy(path_from, path_to)
+				copied_files += 1
 		else:
 			f_path, f_name = os.path.split(path_to)
 			if f_name == "": # for example, if path_to = 'dir1\dir2\'
 				status = "Error!"
 				msg = "Path 'to' is incorrect: '{}'".format(path_to)
-				return {"status": status, "msg": msg}
+				return {"status": status, "msg": msg, "copied_files": copied_files}
 			
 			if os.path.exists(f_path):
 				if not os.path.isdir(f_path):
 					status = "Error!"
 					msg = "Path: '{0}' is not a directory to write file: '{1}'. \n(May be '{2}' it is already existing file?)".format(f_path, f_name, os.path.basename(f_path))
-					return {"status": status, "msg": msg}					
+					return {"status": status, "msg": msg, "copied_files": copied_files}					
 			else:
 				print("Create path: ", f_path)
 				os.makedirs(f_path)
 				
 			print("file-file: write")
 			shutil.copy(path_from, path_to)
+			copied_files += 1
 	
 	elif op_type == "fd":
 		print("file-dir")
 		if not os.path.isfile(path_from):
 			status = "Error!"
 			msg = "Path 'from' is not a file: '{}'".format(path_from)
-			return {"status": status, "msg": msg}
+			return {"status": status, "msg": msg, "copied_files": copied_files}
 		
 		if os.path.exists(path_to):
 			if not os.path.isdir(path_to):
 				status = "Error!"
 				msg = "Path 'to' is not a directory: '{}'".format(path_to)
-				return {"status": status, "msg": msg}
+				return {"status": status, "msg": msg, "copied_files": copied_files}
 			
 			file_name = os.path.basename(path_from)
 			path_to_new = os.path.join(path_to, file_name)
@@ -90,32 +93,35 @@ def copy_with_replace_by_date(path_from, path_to, op_type):
 				if mtime_path_from > mtime_path_to_new:
 					print("file-dir: rewrite")
 					shutil.copy(path_from, path_to_new)
+					copied_files += 1
 			else:
 				print("file-dir: write")
 				shutil.copy(path_from, path_to_new)
+				copied_files += 1
 		else:
 			print("'path_to' not exists")
 			print("Create path: ", path_to)
 			os.makedirs(path_to)
 			print("file-dir: write")
-			shutil.copy(path_from, path_to)	
+			shutil.copy(path_from, path_to)
+			copied_files += 1
 		
 	elif op_type == "df":
 		print("dir-file")
 		status = "Error!"
 		msg = "Write directory to file? Are you really? It doesn't make sense. =)"
-		return {"status": status, "msg": msg}
+		return {"status": status, "msg": msg, "copied_files": copied_files}
 		
 	elif op_type == "dd":
 		print("dir-dir")
 		if not os.path.isdir(path_from):
 			status = "Error!"
 			msg = "Path 'from' is not a directory: '{}'".format(path_from)
-			return {"status": status, "msg": msg}
+			return {"status": status, "msg": msg, "copied_files": copied_files}
 
 		list_path_from = os.listdir(path_from)
-		print(list_path_from)
-		print(len(list_path_from))
+		print("list_path_from: ", list_path_from)
+		print("len(list_path_from): ", len(list_path_from))
 		
 		if len(list_path_from) == 0:
 			# print("path_to: ", path_to)
@@ -124,33 +130,34 @@ def copy_with_replace_by_date(path_from, path_to, op_type):
 				os.makedirs(path_to)
 		
 		for obj in list_path_from:
-			print(obj)
+			print("obj: ", obj)
 			path_from_obj = os.path.join(path_from, obj)
 			path_from_obj = os.path.normpath(path_from_obj)
 			if os.path.isfile(path_from_obj):
 				print("file")
 				res = copy_with_replace_by_date(path_from_obj, path_to, "fd")
 				print(res)
+				copied_files += res["copied_files"]
 			elif os.path.isdir(path_from_obj):
 				print("dir")
 				path_to_obj = os.path.join(path_to, obj)
 				path_to_obj = os.path.normpath(path_to_obj)
 				res = copy_with_replace_by_date(path_from_obj, path_to_obj, "dd")
 				print(res)
+				copied_files += res["copied_files"]
 			else:
 				# print("The object is not supported: ", path_from_obj)
 				status = "Error!"
 				msg = "The object is not supported: '{}'".format(path_from_obj)
-				return {"status": status, "msg": msg}
+				return {"status": status, "msg": msg, "copied_files": copied_files}
 			print("======================")
-			
 			
 	else:
 		status = "Error!"
 		msg = "Operation type is undefined: '{}'".format(op_type)
-		return {"status": status, "msg": msg}
+		return {"status": status, "msg": msg, "copied_files": copied_files}
 			
-	return {"status": status, "msg": msg}
+	return {"status": status, "msg": msg, "copied_files": copied_files}
 
 dir_from = r"test_dir_from"
 dir_from_f1 = r"test_dir_from\myfile.txt"
@@ -163,10 +170,6 @@ dir_to_d3 = r"test_dir_to\dir_3"
 dir_to_d3_er = "test_dir_to\dir_3\\"
 dir_to_d3_f1 = r"test_dir_to\dir_3\myfile.txt"
 
-a = copy_with_replace_by_date("ddd", "test_dir_to\\ddd", "dd")
+a = copy_with_replace_by_date(dir_from, dir_to, "dd")
 print(a)
-
-# add copied files counter
-# fix: copy empty dirs
-# fix: создать каталог, если он не указан в пути назначения (сравнить с хcopy)
 
